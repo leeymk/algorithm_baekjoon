bfb46591e09f9dca07616d3bde87f91e739d3551
leeymk <109324427+leeymk@users.noreply.github.com>
2022-09-26 02:31:12 +0900

    [Silver V] Title: 수 이어가기, Time: 120 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Silver/2635. 수 이어가기/수 이어가기.py
@@ -0,0 +1,20 @@
+first_num = int(input())
+len_result = 0
+result = []
+
+for i in range(first_num+1):
+    result_list = [first_num, i]
+    j = 0
+    while True:
+        last_num = result_list[j] - result_list[j+1]
+        j += 1
+        if last_num < 0:
+            break
+        result_list.append(last_num)
+        if len_result < len(result_list):
+            len_result = len(result_list)
+            result = result_list[:]
+
+print(len_result)
+final_result = [str(result[i]) for i in range(len(result))]
+print(' '.join(final_result))
