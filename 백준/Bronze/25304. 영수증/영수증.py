e83e7cb505b0356f8d1ae937ab021e05d5a821f4
leeymk <109324427+leeymk@users.noreply.github.com>
2022-10-04 22:46:07 +0900

    [Bronze V] Title: 영수증, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+X = int(input())
+N = int(input())
+cnt = 0
+for _ in range(N):
+    a, b = map(int, input().split())
+    cnt += a*b
+if cnt == X:
+    print('Yes')
+else:
+    print('No')
