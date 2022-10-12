3eb7a10d0b57b09684fd9195d781a057a1563da7
leeymk <109324427+leeymk@users.noreply.github.com>
2022-10-12 18:35:55 +0900

    [Gold IV] Title: 플로이드, Time: 680 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -0,0 +1,31 @@
+from sys import stdin
+
+input = stdin.readline
+
+n = int(input()) # 도시의 개수
+m = int(input()) # 버스의 개수
+INF = int(1e9)
+graph = [[INF] * (n+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if i == j:
+            graph[i][j] = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    if graph[a][b] > c:
+        graph[a][b] = c
+
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if graph[i][j] == INF:
+            print(0, end=' ')
+        else:
+            print(graph[i][j], end=' ')
+    print()
